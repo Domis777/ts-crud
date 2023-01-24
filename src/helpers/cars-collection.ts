ba33b34d5a54ec8170b5c612cde720cd22b0cb51
Domixaz <44125565+Domixaz@users.noreply.github.com>
2023-01-24 19:45:56 +0200

    Feat(CAO 12 TS): Add cars-collection constructor

--- src/helpers/cars-collection.ts
+++ src/helpers/cars-collection.ts
@@ -1,9 +1,15 @@
 type CarsCollectionProps = {
-
+    carArr: Car[],
+    brandArr: Brand[],
+    modelArr: Model[] 
 };
 
 class CarsCollection {
-
+    private props: CarsCollectionProps;
+    
+    constructor(props: CarsCollectionProps) {
+        this.props = props;
+    }
 }
 
 export default CarsCollection;
