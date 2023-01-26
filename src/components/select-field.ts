da20996d69c26c2a42155830e76b8c6ccfc56c1a
Domixaz <44125565+Domixaz@users.noreply.github.com>
2023-01-26 09:47:22 +0200

    Feat(CAO 13 TS): Add type SelectFieldProps

--- src/components/select-field.ts
+++ src/components/select-field.ts
@@ -3,12 +3,19 @@ type Option = {
     text: string;
 };
 
+type SelectFieldProps = {
+    options: Option[]
+};
+
 class SelectField {
+    private options: Option[];
+
     public htmlElement: HTMLSelectElement;
 
-    constructor() {
+    constructor({ options }: SelectFieldProps) {
         this.htmlElement = document.createElement('select');
         this.htmlElement.className = 'form-select';
+        this.options = options;
         this.htmlElement.innerHTML = `
         <option value="">BMW</option>
         <option value="">Volvo</option>
