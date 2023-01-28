6b7d28cd299fd4f7fabd24b35e5f65158bfbef30
Domixaz <44125565+Domixaz@users.noreply.github.com>
2023-01-28 21:12:59 +0200

    Feat(CAO 14 TS): Add initialize  "car-form"

--- src/components/car-form.ts
+++ src/components/car-form.ts
@@ -24,45 +24,64 @@ type Fields = {
   };
 
 class CarForm {
-    private props: CarFormProps;
+  private props: CarFormProps;
 
-    public htmlElement: HTMLFormElement;
+  public htmlElement: HTMLFormElement;
 
-    private fields: Fields;
+  private fields: Fields;
 
-    constructor(props: CarFormProps) {
-        this.props = props
-        this.htmlElement = document.createElement('form')
+  private htmlFormHeader: HTMLHeadingElement;
 
-        this.fields = {
-          brand: new SelectField({
-            name: 'brand',
-            labelText: 'Markė',
-            options: brands.map(({ id, title }) => ({ text: title, value: id })),
-          }),
-          model: new SelectField({
-            name: 'model',
-            labelText: 'Modelis',
-            options: models.map(({ id, title }) => ({ text: title, value: id })),
-          }),
-          price: new TextField({
-            name: 'price',
-            labelText: 'Kaina',
-            value: '',
-          }),
-          year: new TextField({
-            name: 'year',
-            labelText: 'Metai',
-            value: '',
-          }),
-        };
+  private htmlFieldsContainer: HTMLDivElement;
 
-        this.initialize();
-        this.renderView();
+  private htmlSubmitBtn: HTMLButtonElement;
+
+  constructor(props: CarFormProps) {
+      this.props = props
+      this.htmlElement = document.createElement('form')
+
+      this.fields = {
+        brand: new SelectField({
+          name: 'brand',
+          labelText: 'Markė',
+          options: brands.map(({ id, title }) => ({ text: title, value: id })),
+        }),
+        model: new SelectField({
+          name: 'model',
+          labelText: 'Modelis',
+          options: models.map(({ id, title }) => ({ text: title, value: id })),
+        }),
+        price: new TextField({
+          name: 'price',
+          labelText: 'Kaina',
+          value: '',
+        }),
+        year: new TextField({
+          name: 'year',
+          labelText: 'Metai',
+          value: '',
+        }),
+      };
+
+      this.initialize();
+      this.renderView();
     };
 
     private initialize() {
+      this.htmlFormHeader.className = 'h3 text-center';
 
+    const fieldsArr = Object.values(this.fields);
+    this.htmlFieldsContainer.className = 'd-flex flex-column gap-2';
+    this.htmlFieldsContainer.append(...fieldsArr.map((field) => field.htmlElement));
+
+    this.htmlSubmitBtn.className = 'btn btn-primary';
+
+    this.htmlElement.className = 'card d-flex flex-column gap-3 p-3';
+    this.htmlElement.append(
+      this.htmlFormHeader,
+      this.htmlFieldsContainer,
+      this.htmlSubmitBtn,
+    );
     };
 
     private renderView() {
