d207044eac1efd7030b3879efd0f11d1992035ee
Domixaz <44125565+Domixaz@users.noreply.github.com>
2023-01-28 16:17:19 +0200

    Feat(CAO 14 TS): Add type Values and type Fields "car-form"

--- src/components/car-form.ts
+++ src/components/car-form.ts
@@ -1,6 +1,27 @@
-type CarFormProps {
+import TextField from './text-field';
+import SelectField from './select-field';
+import brands from '../data/brands';
+import models from '../data/models';
 
-}
+export type Values = {
+    brand: string,
+    model: string,
+    price: string,
+    year: string,
+  };
+
+type CarFormProps {
+    values: Values,
+    title: string,
+    submitBtnText: string,
+};
+
+type Fields = {
+    brand: SelectField,
+    model: SelectField,
+    price: TextField,
+    year: TextField,
+  };
 
 class CarForm {
     private props: CarFormProps;
