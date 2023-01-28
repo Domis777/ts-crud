1a2efd3b41cfc0d64c2bc95953ef19da4cd8ee42
Domixaz <44125565+Domixaz@users.noreply.github.com>
2023-01-28 16:12:02 +0200

    Feat(CAO 14 TS): Add updateProps "car-form"

--- src/components/car-form.ts
+++ src/components/car-form.ts
@@ -8,19 +8,29 @@ class CarForm {
     public htmlElement: HTMLFormElement;
 
     constructor(props: CarFormProps) {
+        this.props = props
         this.htmlElement = document.createElement('form')
 
         this.initialize();
         this.renderView();
-    }
+    };
 
     private initialize() {
 
-    }
+    };
 
     private renderView() {
 
-    }
+    };
+
+    public updateProps(props: Partial<CarFormProps>) {
+        this.props = {
+            ...this.props,
+            ...props,
+        };
+
+        this.renderView();
+    };
 }
 
 export default CarForm;
